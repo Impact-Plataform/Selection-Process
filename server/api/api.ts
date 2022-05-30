4c02fee19c60a711e37d36879000e55a0012f365
Alentejo <arthur.alentejo228@gmail.com>
2022-05-30 01:04:53 -0300

    Adicionando rota ping

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { LoggerController } from '../logger/loggerController';
 import { ManagerService } from '../services/Manager/manager-service';
-
+import log from '../logger/logger';
 const router = express.Router();
 
 var managerService = new ManagerService();
@@ -11,5 +11,12 @@ router.post('/register', managerService.Register);
 //router.post('/test', _ManagerService.SendTest);
 
 router.get('/logs', LoggerController.ReadFile);
+
+router.get('/ping', (req, res) => {
+  const dt = new Date();
+  res.status(200).json({
+    "pong": `${dt.toLocaleTimeString()} - ${dt.toLocaleDateString()}`
+  });
+});
 
 export default router;
