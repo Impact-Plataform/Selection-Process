f9588897fb73a7d816a71656c14ff89ed599ef4b
Alentejo <arthur.alentejo228@gmail.com>
2022-06-04 01:45:43 -0300

    Fixing imports

--- server/services/Registry/registry-service.ts
+++ server/services/Registry/registry-service.ts
@@ -1,16 +1,15 @@
-import Candidate from "./candidate";
+import { Candidate } from "./candidate";
 import { RegistryStorage } from "./registry-storage";
-import { RegistryStorageFake } from "./registry-storageFake";
 
 export class RegistryService{
     
-    public readonly _registryStorage;
+    private readonly _registryStorage;
 
     constructor(){
         this._registryStorage = new RegistryStorage();
     }
 
-    public async CreateCandidate(candidate: Candidate){       
+    public async CreateCandidate(candidate: Candidate){   
         return await this._registryStorage.CreateCandidate(candidate);
     }
 }
