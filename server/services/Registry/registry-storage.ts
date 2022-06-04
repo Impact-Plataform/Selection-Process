5685d54b44e2660d59342196b9adfc35fa4f6427
Alentejo <arthur.alentejo228@gmail.com>
2022-06-04 01:46:14 -0300

    Updating scripts and fixing imports

--- server/services/Registry/registry-storage.ts
+++ server/services/Registry/registry-storage.ts
@@ -1,5 +1,5 @@
 import { Database } from "../common/Database/Database";
-import Candidate from "./candidate";
+import { Candidate } from "./candidate";
 
 export class RegistryStorage{
     private readonly _db: Database;
@@ -7,8 +7,8 @@ export class RegistryStorage{
         this._db = Database.getInstance();
     }
     async CreateCandidate(candidate: Candidate): Promise<any>{        
-        const query = "INSERT INTO candidates (name, birthdate, email, phone) VALUES ($1, $2, $3, $4) RETURNING id";
-        const params = [candidate.name, candidate.birthdate, candidate.email, candidate.phone];
+        const query = "INSERT INTO candidates (name, birthdate, email, phone, how_knew_plataforma) VALUES ($1, $2, $3, $4, $5) RETURNING id";
+        const params = [candidate.name, candidate.birthdate, candidate.email, candidate.phone, candidate.how_knew_plataforma];
         const result = await this._db.run(query, params);
         
         return result;
