c65404251a68280399c0a887b1e21cc32503a6ed
Alentejo <arthur.alentejo228@gmail.com>
2022-06-04 01:45:17 -0300

    Changing candidate to class

--- server/services/Registry/candidate.ts
+++ server/services/Registry/candidate.ts
@@ -1,12 +1,16 @@
-type Candidate = {
-    id: string
-    name: string
-    birthdate: any
-    email: string
-    phone: string
-    interest?: string
-    knowledge?: string
-    how_knew_plataforma?: string
-    speak_english?: string
+export class Candidate {
+    public id: string
+    public name: string
+    public birthdate: string
+    public email: string
+    public phone: string
+    public how_knew_plataforma?: string
+    constructor(payload: any) {
+        this.id = payload.id
+        this.name = payload.name
+        this.birthdate = payload.birthdate
+        this.email = payload.email
+        this.phone = payload.phone
+        this.how_knew_plataforma = payload.how_knew_plataforma
+    }
 }
-export default Candidate
